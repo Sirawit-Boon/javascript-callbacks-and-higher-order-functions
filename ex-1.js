6f78e934dd5150a8ece5bf17374fd87a0f4bc10c
podusit <d.rimdusit@gmail.com>
2024-10-08 15:56:42 +0700

    hackhour:JavaScript Callbacks and Higher Order Functions Assignment

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,21 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for(let i=0;i < array.length; i++){
+    newEmployeeSalaries.push(operation(array[i]));
+  }
+  
 }
 
-const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
+const employeeSalaries = [20005, 40000, 32000, 14500, 344000]; //array
 const newEmployeeSalaries = [];
+
+function getBonus(bonus){ //operation
+  return bonus + 5000;
+  
+}
 
 // Using `forEach` function here
 
+forEach(employeeSalaries,getBonus);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
